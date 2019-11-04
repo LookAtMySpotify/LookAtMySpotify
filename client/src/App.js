bb9e8fb13e684961f6376e3c3119edfa1157d7a2
FichNein <feingon@wwu.edu>
2019-11-04 14:19:02 -0800

    Please Added

--- client/src/App.js
+++ client/src/App.js
@@ -218,7 +218,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <a href='http://localhost:8888' > Login to Spotify </a>
+        <a href='http://localhost:8888' > Please, Login to Spotify </a>
         <div>
           Now Playing: { this.state.nowPlaying.name }
         </div>
